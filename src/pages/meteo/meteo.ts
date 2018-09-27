14a77ac9348861ee3d39c97d728f2fe78fa26bbc
Joe le taxi <julien.vera@hotmail.com>
2018-09-27 18:01:35 +0200

    first modifs

--- src/pages/meteo/meteo.ts
+++ src/pages/meteo/meteo.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the MeteoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @IonicPage()
 @Component({
@@ -14,12 +9,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'meteo.html',
 })
 export class MeteoPage {
+    temperature: number;
+    meteo: string;
+    code_postal: number;
+    url: any;
+    name: string;
+    
+    constructor(public navCtrl: NavController, public navParams: NavParams,
+        public alertCtrl: AlertController, public http: Http) {
+    }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    afficher() {
+        let alert = this.alertCtrl.create({
+            title: 'Error',
+            subTitle: 'Something went wrong :/',
+            buttons: ['OK']
+        });
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MeteoPage');
-  }
+        this.url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + this.code_postal;
+        this.url += ',fr&appid=68a40fffe840bac1f3463b4c9a130473&units=metric&lang=fr';
 
+        this.http.get(this.url).map(result => result.json()).subscribe(
+            data => {
+                this.temperature = data.main.temp;
+                this.meteo = data.weather[0].main;
+                this.name = data.name;
+            },
+            (err) => {
+                alert.present();
+                //TODO: something else ?
+            }
+        );
+    }
 }
